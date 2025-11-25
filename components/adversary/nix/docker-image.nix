d9222f2bb590440a05c4961ca1f588f303fb8f5b
paolino <paolo.veronelli@gmail.com>
2025-11-25 12:26:59 +0000

    [adversary] fix: fix nix image to sleep and publish the antithesis scripts

--- components/adversary/nix/docker-image.nix
+++ components/adversary/nix/docker-image.nix
@@ -1,11 +1,31 @@
 { pkgs, project, version, ... }:
-
-pkgs.dockerTools.buildImage {
+let
+  antithesis-setup = pkgs.runCommand "antithesis-setup" { } ''
+    mkdir -p $out/opt/antithesis/test/v1/
+    cp -r ${../composer}/chain-sync-client  $out/opt/antithesis/test/v1
+    chmod 0755 $out/opt/antithesis/test/v1/*/*
+  '';
+  sleep = pkgs.writeShellScriptBin "sleep-forever" ''
+    #!/bin/sh
+    tail -f /dev/null
+  '';
+  usrBinEnv = pkgs.runCommand "usr-bin-env" {} ''
+    mkdir -p $out/usr/bin
+    ln -s ${pkgs.coreutils}/bin/env $out/usr/bin/env
+  '';
+in pkgs.dockerTools.buildImage {
   name = "ghcr.io/cardano-foundation/cardano-node-antithesis/adversary";
   tag = version;
-  config = { EntryPoint = [ "adversary" ]; };
+  config = { EntryPoint = [ "/bin/sleep-forever" ]; };
   copyToRoot = pkgs.buildEnv {
     name = "image-root";
-    paths = [ project.packages.adversary.package.components.exes.adversary ];
+    paths = [
+      pkgs.coreutils
+      pkgs.bash
+      usrBinEnv
+      project.packages.adversary.package.components.exes.adversary
+      antithesis-setup
+      sleep
+    ];
   };
 }
