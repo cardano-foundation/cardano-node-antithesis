11c60b1dc1b01b5181805220dc2c7ef2d12e3457
Arnaud Bailly <arnaud@pankzsoft.com>
2025-11-26 09:32:56 +0100

    feat: do not try to deserialise txs sent
    This is so because we want to be able to inject malformed transactions
    through the 'adversary', so we need to read txs to inject as pure
    bytestrings and do the minimum to extract txid from them. We could
    also just read a pair (txid, bytes).

---
+++ components/adversary/test/Adversary/SubmitTransactionsSpec.hs
@@ -0,0 +1,15 @@
+module Adversary.SubmitTransactionsSpec where
+
+import Test.Hspec (Spec, it, shouldBe)
+import qualified Data.ByteString.Lazy as LBS
+import Adversary.SubmitTransactions (mkTxId, getTxId, fromHex)
+import Adversary.SubmitTransactions (mkGenTx)
+
+spec :: Spec
+spec = do
+  let txBytes :: LBS.ByteString = fromHex "84a500818258201efc7ffb9772f4425a8edea129b6f08732d3dc41fcb957bfd40d0db68bb33d1600018182581d609749ac0e073844185403a2927239b4e663085e7090d6a4691ac1decf1a00209514021a0005908c031a7fffffff048282008200581c89ca2be8a9b0680dd3293268fe336bf46e2992e52d551b4bdb624a1283028200581c89ca2be8a9b0680dd3293268fe336bf46e2992e52d551b4bdb624a12581c040e1032e2dda149c99fc9d45f06a0457ded6958cd5b2fc2a952ea6ea100828258208c7cc0fcc56e445e28554694060ab5a73c44ceeb76b09530f7126f49f99a63c65840a859678f5d78d8dba62a3f40bc333d4537ae9353de4db0678de8c58158fc4df47185f01cac722133d1f50c465e6ec6155b6763a94acec660c4243f1082bc5b0f8258201751b10751f407febe1f9482514195546e13903e6d63ed2260d03ceb28dcff92584075c78c766d635e4d3953f370af5d2303bb16a25b2f3e59f9a1acd09a293719ff76d2aedc96a7ffa345f8c12faf595895998293087c07e43675b0c1c99b102401f5f6"
+
+  it "can compute transaction id from raw bytes" $ do
+    let txId = mkTxId txBytes
+    let expectedTxId = getTxId <$> mkGenTx txBytes
+    txId `shouldBe` expectedTxId
